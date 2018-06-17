837a85b945806b79ec43efd1d6df30d0c22fdbba
Thibault Rihet <thibault.rihet@trialog.com>
2018-06-17 12:32:44 +0200

    Add query to get city coordinates (not clean at all atm)

--- hello.js
+++ hello.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
+
+var osmium = require('osmium');
 mongoose.connect('mongodb://localhost/leboncoin', function(err) {
-  if (err) { 
-    throw err; 
+  if (err) {
+    throw err;
   }
 });
 
@@ -9,53 +11,87 @@ var searchResultSchema = new mongoose.Schema({
   title: String,
   category: String,
   link: String,
-  images: [ String ],
+  images: [String],
   location: String,
   urgent: Boolean,
   price: Number,
-  date: String,//Date,
+  date: String, //Date,
   id: Number,
   lat: Number,
-  lon: Number
+  lon: Number,
+  city: String
 });
 
-var searchResult = mongoose.model('SearchResult', searchResultSchema);
-searchResult.remove({}, function(err) {
-  if (err) { console.log(err); }
+var citySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  lat: {
+    type: Number,
+    required: true
+  },
+  lon: {
+    type: Number,
+    required: true
+  }
 });
+
+var SearchResult = mongoose.model('SearchResult', searchResultSchema);
+var City = mongoose.model('City', citySchema);
+
+let cleanup = false;
+
+if (cleanup) {
+
+  SearchResult.remove({}, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  City.remove({}, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+}
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
+var express = require('express'); // call express
+var app = express(); // define our app using express
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
-
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    searchResult.find({})
-	.then((results) => {
-		res.json(results);
-	});  
-});
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+// var port = process.env.PORT || 8080; // set our port
+//
+// // ROUTES FOR OUR API
+// // =============================================================================
+// var router = express.Router(); // get an instance of the express Router
+//
+// // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// router.get('/', function(req, res) {
+//   searchResult.find({})
+//     .then((results) => {
+//       res.json(results);
+//     });
+// });
+//
+// // REGISTER OUR ROUTES -------------------------------
+// // all of our routes will be prefixed with /api
+// app.use('/api', router);
+//
+// // START THE SERVER
+// // =============================================================================
+// app.listen(port);
+// console.log('Magic happens on port ' + port);
 
 
 const request = require('superagent');
@@ -64,42 +100,85 @@ const leboncoin = require('leboncoin-api');
 var nPages = 15
 var sleep = require('sleep');
 
-for (let i = 0; i < nPages; i++) {
-	var search = new leboncoin.Search()
-	    .setPage(i)
-	    .setCategory("locations")
-	    .setRegion("rhone_alpes")
-	    .setDepartment("isere")
-	    .addSearchExtra("mrs", 500) // min rent
-	    .addSearchExtra("mre", 1500) // min rent
-	    .addSearchExtra("ret", [1, 3]) // house and garden
 
-	search.run().then(function (data) {
-	    //console.log(data.page); // the current page
-	    //console.log(data.nbResult); // the number of results for this search
-	    //console.log(data.results); // the array of results
-	    if (data.nbResults === 0) { crawledAll = true; }
-	    for (let i = 0; i < data.results.length; i++) {
-		  sleep.usleep(1000000);
-		let insert = new searchResult(data.results[i]);
-		console.log(data.results[i]);
-		let city = data.results[i].location.split(' / ')[0];
-		request
-		  .get('https://nominatim.openstreetmap.org/search')
-		  .query({ q: city + ", isere", format: 'json' }) // query string
-		  .end((err, res) => {
-			if (err) console.log(err);
-			if (res.body.length > 0) {
-				insert.lat = res.body[0].lat;
-				insert.lon = res.body[0].lon;
-			}
-		    	insert.save(function(err){
-			  if(err) { throw err; }
-		   	  console.log("Added #"+i);
-			});
-		  });
-	    }
-	    /*
+// http://api.geonames.org/searchJSON?q=grenoble&country=FR&username=7hibault&featureClass=PPL
+
+
+
+for (let i = 0; i < nPages; i++) {
+  var search = new leboncoin.Search()
+    .setPage(i)
+    .setCategory("locations")
+    .setRegion("rhone_alpes")
+    .setDepartment("isere")
+    .addSearchExtra("mrs", 500) // min rent
+    .addSearchExtra("mre", 1500) // min rent
+    .addSearchExtra("ret", [1]) // house and garden (3)
+
+  search.run().then(function(data) {
+    // console.log(data)
+    // console.log(data.page); // the current page
+    // console.log(data.nbResult); // the number of results for this search
+    //console.log(data.results); // the array of results
+    if (data.nbResults === 0) {
+      crawledAll = true;
+    }
+    // for (let i = 0; i < data.results.length; i++) {
+    // console.log(data.results[i]);
+    // sleep.usleep(1000000);
+    // let insert = new searchResult(data.results[i]);
+    let foundCity = "voreppe"; //data.results[i].location.split(' / ')[0];
+    City
+      .findOne({
+        city: foundCity
+      })
+      .then((city) => {
+        var p = new mongoose.Promise();
+        if (!city) {
+          request
+            .get('http://api.geonames.org/searchJSON')
+            .query({
+              q: city,
+              country: 'FR',
+              username: "7hibault"
+            }) // query string
+            .end((err, res) => {
+              if (err) console.log(err);
+              let obj = {
+                name: foundCity
+              }
+              if (res.body.geonames.length > 0) {
+                obj.lat = Number(res.body.geonames[0].lat);
+                obj.lon = Number(res.body.geonames[0].lng);
+              }
+              let newCity = new City(obj);
+              newCity.save(function(err) {
+                if (err) console.log(err);
+                console.log("inserted " + foundCity);
+                p.fulfill(newCity);
+              });
+            });
+        } else {
+          console.log("city " + city.name + " already found");
+          p.fulfill(city);
+        }
+        return p;
+      })
+      .then((city) => {
+        let result = {
+          city: city.name
+        }
+        let insertResult = new SearchResult(result);
+        insertResult.save(function(err) {
+          if (err) console.log(err);
+          else console.log("inserted " + insertResult);
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // }
+    /*
 	    data.results[0].getDetails().then(function (details) {
 		console.log(details); // the item 0 with more data such as description, all images, author, ...
 	    }, function (err) {
@@ -108,14 +187,12 @@ for (let i = 0; i < nPages; i++) {
 	    data.results[0].getPhoneNumber().then(function (phoneNumer) {
 		console.log(phoneNumer); // the phone number of the author if available
 	    }, function (err) {
-		console.error(err); // if the phone number is not available or not parsable (image -> string) 
+		console.error(err); // if the phone number is not available or not parsable (image -> string)
 	    });
 	    */
-	}, function (err) {
-	    console.error(err);
-	});
+  }, function(err) {
+    console.error(err);
+  });
 
 
 }
-
-
