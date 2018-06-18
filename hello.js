d18bc33b16037d6d4a65c9a0fe5e49b39bb327e9
Thibault Rihet <thibault.rihet@trialog.com>
2018-06-19 01:20:47 +0200

    First working version

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),  Schema = mongoose.Schema;
+var cors = require('cors')
 
-  mongoose.connect('mongodb://localhost/leboncoin', function(err) {
+mongoose.connect('mongodb://localhost/leboncoin', function(err) {
   if (err) {
     throw err;
   }
@@ -18,7 +19,10 @@ var searchResultSchema = new mongoose.Schema({
   id: Number,
   lat: Number,
   lon: Number,
-  city: String
+  city: {
+    type: Schema.Types.ObjectId,
+    ref: 'City'
+  },
 });
 
 var citySchema = new mongoose.Schema({
@@ -27,12 +31,10 @@ var citySchema = new mongoose.Schema({
     required: true
   },
   lat: {
-    type: Number,
-    required: true
+    type: Number
   },
   lon: {
-    type: Number,
-    required: true
+    type: Number
   }
 });
 
@@ -40,6 +42,7 @@ var SearchResult = mongoose.model('SearchResult', searchResultSchema);
 var City = mongoose.model('City', citySchema);
 
 let cleanup = false;
+let searchBool = false;
 
 if (cleanup) {
 
@@ -68,6 +71,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
+app.use(cors());
 
 var port = process.env.PORT || 8080; // set our port
 
@@ -78,6 +82,7 @@ var router = express.Router(); // get an instance of the express Router
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
   SearchResult.find({})
+    .populate('city')
     .then((results) => {
       res.json(results);
     });
@@ -100,81 +105,83 @@ var nPages = 15
 // var sleep = require('sleep');
 
 
-// http://api.geonames.org/searchJSON?q=grenoble&country=FR&username=7hibault&featureClass=PPL
+// http://api.geonames.org/searchJSON?q=grenoble&country=FR&username=7hibault
 
 var globalCounter = 0;
 
-for (let i = 0; i < nPages; i++) {
-  var search = new leboncoin.Search()
-    .setPage(i)
-    .setCategory("locations")
-    .setRegion("rhone_alpes")
-    .setDepartment("isere")
-    .addSearchExtra("mrs", 500) // min rent
-    .addSearchExtra("mre", 1500) // max rent
-    .addSearchExtra("ret", [1]) // house and garden (3)
 
-  search.run().then(function(data) {
-    if (data.nbResults === 0) {
-      crawledAll = true;
-    }
-    for (let i = 0; i < data.results.length; i++) {
-      let foundCity = data.results[i].location.split(' / ')[0];
-      City
-        .findOne({
-          city: foundCity
-        })
-        .then((city) => {
-          var p = new mongoose.Promise();
-          if (!city) {
-            request
-              .get('http://api.geonames.org/searchJSON')
-              .query({
-                q: city,
-                country: 'FR',
-                username: "7hibault"
-              }) // query string
-              .end((err, res) => {
-                if (err) console.log(err);
-                let obj = {
-                  name: foundCity
-                }
-                if (res.body.geonames.length > 0) {
-                  obj.lat = Number(res.body.geonames[0].lat);
-                  obj.lon = Number(res.body.geonames[0].lng);
-                }
-                let newCity = new City(obj);
-                newCity.save(function(err) {
-                  if (err) console.log(err);
-                  // console.log("inserted " + foundCity);
-                  p.fulfill(newCity);
-                });
-              });
-          } else {
-            // console.log("city " + city.name + " already found");
-            p.fulfill(city);
-          }
-          return p;
-        })
-        .then((city) => {
-          let result = {
-            city: city.name,
-            price: data.results[i].price,
-            link: data.results[i].link
-          }
-          let insertResult = new SearchResult(result);
-          insertResult.save(function(err) {
-            if (err) console.log(err);
-            else console.log(globalCounter++);
+if (searchBool) {
+  for (let i = 0; i < nPages; i++) {
+    var search = new leboncoin.Search()
+      .setPage(i)
+      .setCategory("locations")
+      .setRegion("rhone_alpes")
+      .setDepartment("isere")
+      .addSearchExtra("mrs", 500) // min rent
+      .addSearchExtra("mre", 1500) // max rent
+      .addSearchExtra("ret", [1]) // house and garden (3)
+
+    search.run().then(function(data) {
+      if (data.nbResults === 0) {
+        crawledAll = true;
+      }
+      for (let i = 0; i < data.results.length; i++) {
+        let foundCity = data.results[i].location.split(' / ')[0];
+        City
+          .findOne({
+            city: foundCity
           })
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  }, function(err) {
-    console.error(err);
-  });
-
-
+          .then((city) => {
+            var p = new mongoose.Promise();
+            if (!city) {
+              request
+                .get('http://api.geonames.org/searchJSON')
+                .query({
+                  q: foundCity,
+                  country: 'FR',
+                  username: "7hibault"
+                }) // query string
+                .end((err, res) => {
+                  if (err) console.log(err);
+                  let obj = {
+                    name: foundCity
+                  }
+                  if (res.body.geonames && res.body.geonames.length > 0) {
+                    obj.lat = Number(res.body.geonames[0].lat);
+                    obj.lon = Number(res.body.geonames[0].lng);
+                  }
+                  let newCity = new City(obj);
+                  newCity.save(function(err) {
+                    if (err) console.log(err);
+                    // console.log("inserted " + foundCity);
+                    p.fulfill(newCity);
+                  });
+                });
+            } else {
+              // console.log("city " + city.name + " already found");
+              p.fulfill(city);
+            }
+            return p;
+          })
+          .then((city) => {
+            let result = {
+              city: city,
+              price: data.results[i].price,
+              link: data.results[i].link,
+              images: data.results[i].images
+            }
+            let insertResult = new SearchResult(result);
+            insertResult.save(function(err) {
+              if (err) console.log(err);
+              else console.log(globalCounter++);
+            })
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    }, function(err) {
+      console.error(err);
+    });
+  }
 }
